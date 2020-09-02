fbeea11a56228e83f90538902b8e1be67793bda0
Xymist <james@jamieduerden.me>
2020-09-02 18:28:31 +0100

    Fix siblings

--- src/siblings.js
+++ src/siblings.js
@@ -1,11 +1,22 @@
 const deselectSiblings = (elem) => {
-  let sibling = elem.parentNode.firstChild;
+  // Find the row this belongs in, and grab its first column, as
+  // the columns are the direct children
+  let sibling_column = elem.closest('.et_pb_row').firstElementChild;
 
-  while (sibling) {
-    if (sibling.classList.includes('selectedOption') && sibling !== elem) {
-      siblings.classList.remove('selectedOption');
+  // If there's another column to consider, keep going
+  while (sibling_column) {
+    // Grab the first important child of that column, which will be
+    // the named div we're shadowing
+    let sibling = sibling_column.firstElementChild;
+
+    // If it exists and is selected, and is not the thing that was just clicked,
+    // deselect it.
+    if (sibling && sibling.classList.includes('selectedOption') && sibling !== elem) {
+      sibling.classList.remove('selectedOption');
     }
-    sibling = sibling.nextSibling
+
+    // Move to the next column in the row and repeat the loop
+    sibling_column = sibling_column.nextSibling
   }
 };
 
